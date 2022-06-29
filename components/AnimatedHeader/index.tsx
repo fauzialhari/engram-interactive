4121a747ee7e2375c3780b2095b8917f5eeb9a19
Fauzi Alhari <fauzialhari@gmail.com>
2022-06-30 06:15:39 +0700

    Create skeleton of component animatedHeader

---
+++ components/AnimatedHeader/index.tsx
@@ -0,0 +1,8 @@
+const AnimatedHeader: React.FC<{
+  text: string;
+  subtitle?: string;
+  isLeftPositioned?: boolean;
+}> = ({ text, isLeftPositioned = true, subtitle }) => {
+};
+
+export default AnimatedHeader;
