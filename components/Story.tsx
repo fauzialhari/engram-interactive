af773026d3e129c330b9d32de6527fe37c76a60e
Fauzi Alhari <fauzialhari@gmail.com>
2022-06-30 16:44:41 +0700

    Create skeleton of section story

---
+++ components/Story.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { useLayoutEffect, useRef } from "react";
+const Story: React.FC<{
+  intro: string;
+  title: string;
+  background: string;
+  children: JSX.Element;
+}> = ({ intro, title, background, children }) => {
+};
+
+export default Story;
