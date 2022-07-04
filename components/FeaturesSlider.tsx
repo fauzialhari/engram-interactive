b51aa258abf0cfe2a35d1144e6414a7728eca4cd
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-05 06:51:27 +0700

    Refine layout of featuresSlider

--- components/FeaturesSlider.tsx
+++ components/FeaturesSlider.tsx
@@ -1,5 +1,6 @@
 import { useState, useCallback } from "react";
 import Image from "next/image";
+import AnimatedHeader from "./AnimatedHeader";
 import Carousel from "./Carousel";
 const FeaturesSlider: React.FC<{
   featureImages: {
@@ -13,20 +14,26 @@ const FeaturesSlider: React.FC<{
     setActiveSlide(CarouselActiveSlide);
   }, []);
   return (
-    <Carousel animateChildren={animate}>
-      {featureImages.map(({ url, id, title }, index) => {
-        return (
-          <div
-            className={`${
-              index === activeSlide ? "" : "opacity-0 "
-            }h-full flex justify-center items-center transtion-opacity duration-250`}
-            key={id}
-          >
-            <Image src={url} width="1260" height="637" alt={title} />
-          </div>
-        );
-      })}
-    </Carousel>
+    <section className="px-24 mb-[26rem]">
+      <AnimatedHeader
+        text="Features"
+        subtitle="variety of mod chips & psionics to choose"
+      />
+      <Carousel animateChildren={animate}>
+        {featureImages.map(({ url, id, title }, index) => {
+          return (
+            <div
+              className={`${
+                index === activeSlide ? "" : "hidden "
+              }h-full flex justify-center items-center transtion-opacity duration-250`}
+              key={id}
+            >
+              <Image src={url} width="1260" height="637" alt={title} />
+            </div>
+          );
+        })}
+      </Carousel>
+    </section>
   );
 };
 
