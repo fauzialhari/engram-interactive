a1c9d905e6a3873865b0fd6c57d4565c13b9aad2
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-05 06:54:06 +0700

    Create skeleton for component CharactersSlider

---
+++ components/CharactersSlider.tsx
@@ -0,0 +1,11 @@
+const CharactersSlider: React.FC<{
+  charactersContent: {
+    characterImageUrl: string;
+    id: string;
+    title: string;
+    description: string;
+  }[];
+}> = ({ charactersContent }) => {
+};
+
+export default CharactersSlider;
