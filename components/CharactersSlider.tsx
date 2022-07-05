94d8f156cf857ac719bc0ddf03f749d0da5a59dc
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-05 09:28:16 +0700

    Add layout for copywriting part of CractersSlider

--- components/CharactersSlider.tsx
+++ components/CharactersSlider.tsx
@@ -42,10 +42,30 @@ const CharactersSlider: React.FC<{
                       />
                     </div>
                   </div>
-                  <div className="relative w-1/2">
-                    <div className="absolute">
-                      <h2>{title}</h2>
-                      {description}
+                  <div className="w-1/2 flex items-end">
+                    <div className="relative inline-block px-9 py-8">
+                      <span
+                        className="
+                          absolute inline-block h-full w-8 inset-y-0 left-0
+                          before:content-[''] before:border-t-2 before:border-l-2 
+                          before:border-primary before:absolute before:w-full before:h-8 
+                          before:top-0 before:inset-x-0 after:content-[''] after:border-b-2 
+                          after:border-l-2 after:border-primary after:absolute after:w-full 
+                          after:h-8 after:bottom-0 after:inset-x-0"
+                      ></span>
+                      <div className="bg-secondary py-12 px-[4.375rem]">
+                        <h2>{title}</h2>
+                        <p className="">{description}</p>
+                      </div>
+                      <span
+                        className="
+                          absolute inline-block h-full w-8 inset-y-0 right-0 
+                          before:content-[''] before:border-t-2 before:border-r-2 
+                          before:border-primary before:absolute before:w-full before:h-8 
+                          before:top-0 before:inset-x-0 after:content-[''] after:border-b-2 
+                          after:border-r-2 after:border-primary after:absolute after:w-full 
+                          after:h-8 after:bottom-0 after:inset-x-0"
+                      ></span>
                     </div>
                   </div>
                 </div>
