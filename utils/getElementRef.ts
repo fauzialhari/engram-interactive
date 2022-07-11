6bde46b6a9b17abae0536e363f66f7a996802e7f
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-11 10:39:41 +0700

    Make ref checking be more general in getElementRef

--- utils/getElementRef.ts
+++ utils/getElementRef.ts
@@ -1,6 +1,6 @@
 import { RefObject } from "react";
 export default function getElement(RefElement: RefObject<HTMLElement>){
-    if (RefElement.current !== null) {
+    if (!!RefElement.current) {
         return RefElement.current
     }
     throw new Error("Cannot find ELement " + RefElement);
