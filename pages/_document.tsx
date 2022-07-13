52284260588f378e5ff65e49ad987e4f6e38159d
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-13 08:03:22 +0700

    Implement meta viewport

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
-        <body className="bg-mainbg text-white text-2xl overflow-x-hidden">
+        <Head>
+          <meta name="viewport" content="width=device-width, minimum-scale=1" />
+          met
+        </Head>
+        <body className="bg-mainbg text-white lg:text-2xl overflow-x-hidden">
           <Main />
           <NextScript />
         </body>
