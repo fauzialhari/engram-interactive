83acb967c46cc3130f9bb6aaf8055a3045534c91
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-14 09:05:56 +0700

    Add images domain

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['fc.engraminteractive.com'],
+  },
 }
 
 module.exports = nextConfig
