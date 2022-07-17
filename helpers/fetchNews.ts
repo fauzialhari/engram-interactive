01c34398220e4ab5dc17723f7bc08b329c6599a2
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-17 09:59:22 +0700

    Update function fetchNews to handle error fetching

--- helpers/fetchNews.ts
+++ helpers/fetchNews.ts
@@ -1,26 +1,46 @@
-async function fetchNews(page: number) {
-  const newsResponse = await fetch(
-    `https://fc.engraminteractive.com/wp-json/wp/v2/posts?page=${page}&per_page=2`
-  );
-  const newsJSON = await newsResponse.json();
-  return newsJSON.map(
-    ({
-      title,
-      date,
-      content,
-      id,
-    }: {
-      title: { rendered: string };
+async function fetchNews({ page }: { page: number }): Promise<
+  | {
+      title: string;
       date: string;
-      content: { rendered: string };
-      id: number;
-    }) => ({
-      title: typeof title === "string" ? "" : title.rendered,
-      date,
-      content: typeof content === "string" ? "" : content.rendered,
-      id,
-    })
-  );
+      content: string;
+      id: string;
+    }[]
+  | string
+> {
+  try {
+    const newsResponse = await fetch(
+      `https://fc.engraminteractive.com/wp-json/wp/v2/posts?page=${page}&per_page=2`
+    );
+    if (!newsResponse.ok) {
+      if (newsResponse.status === 400) {
+        throw new Error("There is no more news to show");
+      }else {
+        throw new Error("Fail to load news, please try again later");
+      }
+    }
+
+    const newsJSON = await newsResponse.json();
+    return newsJSON.map(
+      ({
+        title,
+        date,
+        content,
+        id,
+      }: {
+        title: { rendered: string };
+        date: string;
+        content: { rendered: string };
+        id: number;
+      }) => ({
+        title: typeof title === "string" ? "" : title.rendered,
+        date,
+        content: typeof content === "string" ? "" : content.rendered,
+        id,
+      })
+    );
+  } catch (error) {
+    throw error;
+  }
 }
 
 export default fetchNews;
