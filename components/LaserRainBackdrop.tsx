40c44afaa82288b28469395383626c15fc2f8f2f
Fauzi Alhari <fauzialhari@gmail.com>
2022-07-19 11:37:49 +0700

    Make laser backdrop animation smoother - Change from FpsCtrl to requestAnnimationFrame to smoother transition - Change from onScrollDetection to IntersectionObserver for better viewport change - Make laser movement speed value and color varies based on level - Make fading effect on laser dispatching

--- components/LaserRainBackdrop.tsx
+++ components/LaserRainBackdrop.tsx
@@ -4,14 +4,11 @@ import createCanvasLaserElement, {
   LaserElement,
 } from "../helpers/createCanvasLaserElement";
 import randomIntFromInterval from "../utils/randomIntFromInterval";
-import detectScrollDirection from "../utils/detectScrollDirection";
-console.log(detectScrollDirection);
 
-import FpsCtrl from "../helpers/FpsCtrl";
 const LaserRainBackdrop: React.FC<{
   laserDistance?: number;
   movementSpeed?: number;
-}> = ({ laserDistance = 60, movementSpeed = 20 }) => {
+}> = ({ laserDistance = 30, movementSpeed = 5 }) => {
   const canvasRef = useRef(null);
   useLayoutEffect(() => {
     const canvasElement = getElementRef(canvasRef) as HTMLCanvasElement;
@@ -19,40 +16,52 @@ const LaserRainBackdrop: React.FC<{
     canvasElement.setAttribute("height", `${window.innerHeight}`);
     // initializing canvas
     const canvasContext = canvasElement.getContext("2d");
-    const PRIMARY_COLOR = "#03c7ff";
-    const SECONDARY_COLOR = "#2e7299";
+
     let lasers: LaserElement[] = [];
-    function generateLasers() {
+    function generateLasers(emiterConstant: number) {
       if (!!canvasContext) {
-        let laserGenerationArea = 0;
+        let laserGenerationAreaBegin = 0;
+
         do {
+          const laserGenerationAreaEnd =
+            laserDistance * emiterConstant + laserGenerationAreaBegin;
           const laserGenerationPoint = randomIntFromInterval(
-            laserGenerationArea,
-            laserGenerationArea + laserDistance
+            laserGenerationAreaBegin,
+            laserGenerationAreaEnd
           );
-          const color = randomIntFromInterval(0, 1)
-            ? PRIMARY_COLOR
-            : SECONDARY_COLOR;
           lasers.push(
-            createCanvasLaserElement(canvasContext, laserGenerationPoint, color)
+            createCanvasLaserElement(canvasContext, laserGenerationPoint)
           );
-          laserGenerationArea += laserDistance;
-        } while (laserGenerationArea < canvasElement.width);
+          laserGenerationAreaBegin = laserGenerationAreaEnd;
+        } while (laserGenerationAreaBegin < canvasElement.width);
       }
     }
     let isPaused = false;
-    window.addEventListener("scroll", (e) => {
-      if (detectScrollDirection() === "down") {
-          isPaused = true;
-      } else {
-          isPaused = false;
+    let distanceConstant = 1;
+    let observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.intersectionRatio < 0.4) {
+            isPaused = true;
+          } else {
+            isPaused = false;
+            distanceConstant = Math.pow(1 / entry.intersectionRatio, 8);
+          }
+        });
+      },
+      {
+        threshold: [
+          1, 0.95, 0.9, 0.85, 0.8, 0.75, 0.7, 0.65, 0.6, 0.55, 0.5, 0.45, 0.4,
+          0.35, 0.3, 0.25, 0.25, 0.2, 0.15, 0.1, 0.05, 0,
+        ],
       }
-    });
+    );
+    observer.observe(canvasElement);
 
     //start animation
     let animation: number;
     let distanceMovement = 0;
-    const laserRainAnimation = new FpsCtrl(36, () => {
+    const animate = () => {
       if (!isPaused) {
         if (!!canvasContext) {
           // clear canvas before repainting
@@ -64,19 +73,20 @@ const LaserRainBackdrop: React.FC<{
           );
         }
         distanceMovement += movementSpeed;
-        if (distanceMovement > 400) {
-          generateLasers();
+        const thresholdToGenerateLasers = 250;
+        if (distanceMovement > thresholdToGenerateLasers) {
+          generateLasers(distanceConstant);
           distanceMovement = 0;
         }
         lasers = lasers.filter((laser) => {
-          laser.animate(movementSpeed);
-          return (
-            laser.endTipPosition <=
-            (randomIntFromInterval(75, 99) / 100) * canvasElement.height
-          );
+          return !laser.animate(movementSpeed).finished;
         });
+      } else {
+        cancelAnimationFrame(animation);
       }
-    });
+      animation = requestAnimationFrame(animate);
+    };
+    animate();
   });
   return <canvas ref={canvasRef} id="laser-rain"></canvas>;
 };
--- helpers/createCanvasLaserElement.ts
+++ helpers/createCanvasLaserElement.ts
@@ -5,40 +5,71 @@ export type LaserElement = {
   beginTipPosition: number;
   height: number;
   width: number;
+  level: number;
+  opacity: number;
   color: string;
   endTipPosition: number;
+  finished: boolean;
   draw: () => LaserElement;
   animate: (distanceMovement: number) => LaserElement;
 };
 
+const createPrimarycolor = (opacity: number) => `rgba(3, 199, 255, ${opacity})`;
+const createSecondarycolor = (opacity: number) =>
+  `rgba(33, 120, 163, ${opacity})`;
+const createTertiarycolor = (opacity: number) =>
+  `rgba(11, 74, 106, ${opacity})`;
+const generateColors = (opacity:number) => [
+  createTertiarycolor(opacity),
+  createSecondarycolor(opacity),
+  createPrimarycolor(opacity),
+];
+
 const createLaser = (
   canvasContext: CanvasRenderingContext2D,
-  xPosition: number,
-  color: string
-): LaserElement => ({
-  xPosition,
-  beginTipPosition: randomIntFromInterval(-1000, -400),
-  height: randomIntFromInterval(250, 400),
-  width: randomIntFromInterval(1, 3),
-  color,
-  get endTipPosition() {
-    return this.beginTipPosition + this.height;
-  },
-  draw() {
-    canvasContext.strokeStyle = this.color;
-    canvasContext.lineWidth = this.width;
-    canvasContext.shadowBlur = 1; //shadow blur always 5
-    canvasContext.shadowColor = this.color;
-    canvasContext.beginPath();
-    canvasContext.moveTo(this.xPosition, this.beginTipPosition);
-    canvasContext.lineTo(this.xPosition, this.endTipPosition);
-    canvasContext.stroke();
-    return this;
-  },
-  animate(distanceMovement: number) {
-    this.beginTipPosition += distanceMovement;
-    return this.draw();
-  },
-});
+  xPosition: number
+): LaserElement => {
+  return {
+    finished: false,
+    xPosition,
+    beginTipPosition: randomIntFromInterval(-1000, -400),
+    height: randomIntFromInterval(250, 400),
+    width: randomIntFromInterval(1, 3),
+    level: randomIntFromInterval(1, 3),
+    opacity: 1,
+    get color() {
+      return generateColors(this.opacity)[this.level-1];
+    },
+    get endTipPosition() {
+      return this.beginTipPosition + this.height;
+    },
+    draw() {
+      // ending area is aout 75 to 99 % of canvas height
+      const laserEndingArea =
+        (randomIntFromInterval(75, 99) / 100) * canvasContext.canvas.height;
+      const isEndTipEnteringEndingArea = this.endTipPosition >= laserEndingArea;
+      if (isEndTipEnteringEndingArea) {
+        // reduce opacity on entering ending area
+        this.opacity -=0.25;
+        this.finished = this.opacity <= 0;
+      }
+
+      canvasContext.strokeStyle = this.color;
+      canvasContext.lineWidth = this.width;
+      canvasContext.shadowBlur = 1; //shadow blur always 5
+      canvasContext.shadowColor = this.color;
+      canvasContext.beginPath();
+      canvasContext.moveTo(this.xPosition, this.beginTipPosition);
+      canvasContext.lineTo(this.xPosition, this.endTipPosition);
+      canvasContext.stroke();
+      return this;
+    },
+    animate(distanceMovement: number) {
+      // make movement value is calculated based on level
+      this.beginTipPosition += distanceMovement * this.level;
+      return this.draw();
+    },
+  };
+};
 
 export default createLaser;
