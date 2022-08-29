ef7f429156be3ab04f6b5283811b6c3cc20466c8
Fauzi Alhari <32444439+fauzialhari@users.noreply.github.com>
2022-08-29 10:30:45 +0700

    Add Favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,26 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html className="3xl:text-3xl 4xl:text-4xl 5xl:text-5xl 6xl:text-6xl">
-        <Head />
+        <Head>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+        </Head>
         <body className="bg-mainbg text-white lg:text-lg overflow-x-hidden">
           <Main />
           <NextScript />
